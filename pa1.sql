b1f84e56d0835d0538bf8e0724de6f0254817a90
Anna <123003616+Click41@users.noreply.github.com>
2024-02-01 15:10:54 -0500

    FIRST TABLE

---
+++ pa1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Motion_Picture (
+    m_id CHAR(),
+    name STRING,
+    genre STRING,
+    production STRING,
+    release_date DATE,
+    budget INT,
+    ratings FLOAT(0, 10),
+    PRIMARY KEY (m_id)
+)
+
